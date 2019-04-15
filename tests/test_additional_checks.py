b25b4b66a0b4982e6228b9e14fe959766d0fea97
Bibiana Cristofol Amat <bcristofol@gmail.com>
2019-04-15 12:20:30 +0100

    Add more additional checks tests - empty fields

--- tests/test_additional_checks.py
+++ tests/test_additional_checks.py
@@ -3,7 +3,7 @@ import json
 from libcoveocds.lib.additional_checks import run_additional_checks, TEST_CLASSES
 
 
-def test_empty_fields():
+def test_empty_fields_basic():
     with open('./tests/fixtures/additional_checks/empty_fields.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
@@ -15,3 +15,45 @@ def test_empty_fields():
     ]
 
     assert additional_checks == result
+
+
+def test_empty_fields_empty_string():
+    data = {
+        "releases": [
+            {
+                "ocid": "ocds-213czf-000-00001",
+                "date": "",
+                "initiationType": "tender",
+            }
+        ]
+    }
+
+    assert (
+        run_additional_checks(data, TEST_CLASSES['additional']) ==
+        [{'json_location': 'releases/0/date', 'type': 'empty_field'}]
+    )
+
+
+def test_empty_fields_empty_dict():
+    data = {"releases": [{"buyer": {}}]}
+
+    assert (
+            run_additional_checks(data, TEST_CLASSES['additional']) ==
+            [{'json_location': 'releases/0/buyer', 'type': 'empty_field'}]
+    )
+
+
+def test_empty_fields_empty_list():
+    data = {"releases": [{"parties": []}]}
+
+    assert (
+            run_additional_checks(data, TEST_CLASSES['additional']) ==
+            [{'json_location': 'releases/0/parties', 'type': 'empty_field'}]
+    )
+
+
+def test_empty_fields_all_fine():
+    with open('./tests/fixtures/additional_checks/basic_1.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    assert run_additional_checks(data, TEST_CLASSES['additional']) == []
