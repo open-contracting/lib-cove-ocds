d6e5bd0ef456418a52392657225116591bf66321
Bibiana Cristofol Amat <bcristofol@gmail.com>
2019-05-03 14:31:50 +0100

    [901] Additional fields - Check empty fields in records

--- libcoveocds/lib/additional_checks.py
+++ libcoveocds/lib/additional_checks.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 import libcove.lib.tools as tools
 
 
-def flatten_dict(release, path=""):
-    for key, value in sorted(release.items()):
+def flatten_dict(data, path=""):
+    for key, value in sorted(data.items()):
         if isinstance(value, list):
             if len(value) == 0:
                 yield ("{}/{}".format(path, key), value)
@@ -23,11 +23,10 @@ def flatten_dict(release, path=""):
 
 class AdditionalCheck():
     def __init__(self, **kw):
-        self.releases = kw.get('releases')
         self.failed = False
         self.output = []
 
-    def process(self, release, path_prefix):
+    def process(self, data, path_prefix):
         pass
 
 
@@ -41,10 +40,10 @@ class EmptyFieldCheck(AdditionalCheck):
             'json_location': path_prefix + key
         })
 
-    def process(self, release, path_prefix):
-        flattened_release = OrderedDict(flatten_dict(release))
+    def process(self, data, path_prefix):
+        flattened_data = OrderedDict(flatten_dict(data))
 
-        for key, value in flattened_release.items():
+        for key, value in flattened_data.items():
             if isinstance(value, str) and len(value.strip()) == 0:
                 self.update_object(path_prefix, key)
             elif isinstance(value, dict) and len(value) == 0:
@@ -77,14 +76,23 @@ def get_additional_checks_results(test_instances):
     return results
 
 
+def get_file_type_records_or_releases(json_data):
+    if json_data.get('releases'):
+        return 'releases'
+    return 'records'
+
+
 @tools.ignore_errors
 def run_additional_checks(json_data, test_classes):
-    if 'releases' not in json_data:
+    if json_data.get('releases') is None and json_data.get('records') is None:
         return []
-    test_instances = [test_cls(releases=json_data['releases']) for test_cls in test_classes]
 
-    for num, release in enumerate(json_data['releases']):
+    file_type = get_file_type_records_or_releases(json_data)
+
+    test_instances = [test_cls(data=json_data[file_type]) for test_cls in test_classes]
+
+    for num, data in enumerate(json_data[file_type]):
         for test_instance in test_instances:
-            test_instance.process(release, 'releases/{}'.format(num))
+            test_instance.process(data, '{}/{}'.format(file_type, num))
 
     return get_additional_checks_results(test_instances)
