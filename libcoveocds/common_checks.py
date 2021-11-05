3397696023ecedb405e0cd56c3dc7112cb2633d2
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-05 18:32:15 -0400

    chore: Remove noqa comment

--- libcoveocds/common_checks.py
+++ libcoveocds/common_checks.py
@@ -19,7 +19,8 @@ md = MarkdownIt()
 
 validation_error_lookup = {
     "date-time": mark_safe(
-        'Incorrect date format. Dates should use the form YYYY-MM-DDT00:00:00Z. Learn more about <a href="https://standard.open-contracting.org/latest/en/schema/reference/#date">dates in OCDS</a>.'  # noqa: E501
+        'Incorrect date format. Dates should use the form YYYY-MM-DDT00:00:00Z. Learn more about '
+        '<a href="https://standard.open-contracting.org/latest/en/schema/reference/#date">dates in OCDS</a>.'
     ),
 }
 
