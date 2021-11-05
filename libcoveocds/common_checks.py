5a52b6ba7a7da04b237787fff0a7813688f8211c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-05 18:47:52 -0400

    chore: black

--- libcoveocds/common_checks.py
+++ libcoveocds/common_checks.py
@@ -19,7 +19,7 @@ md = MarkdownIt()
 
 validation_error_lookup = {
     "date-time": mark_safe(
-        'Incorrect date format. Dates should use the form YYYY-MM-DDT00:00:00Z. Learn more about '
+        "Incorrect date format. Dates should use the form YYYY-MM-DDT00:00:00Z. Learn more about "
         '<a href="https://standard.open-contracting.org/latest/en/schema/reference/#date">dates in OCDS</a>.'
     ),
 }
